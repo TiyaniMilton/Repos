f959a6c801dd347149cf949cb47c2dc368b90714
Tiyani <tiyanimilton@hotmail.com>
2016-06-09 15:10:28 +0200

    Test Conmint 2 push

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace WindowsFormsApplication1
     {
         /// <summary>
         /// The main entry point for the application.ddfd wqewqweasds
-        /// TEST COMMIT
+        /// TEST COMMIT 2
         /// </summary>
         [STAThread]
         static void Main()
